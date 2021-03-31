4f69a519342e3a36ce7fa8ac510c2b57fa4c4191
alina solkar <alinasolkar6@gmail.com>
2021-03-31 06:34:20 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
-function compute()
+function compute(){
+//get the value and calculate
+   var principal =parseFloat( document.getElementById("principal").value);
+   var rate=parseFloat(document.getElementById("rate").value);
+   var years=parseFloat(document.getElementById("years").value);
+   var interest = principal*years*rate/100;
+   var yearInFuture= new Date().getFullYear()+years;
+   //create the interest rate
+   document.getElementById("result").innerHTML = "Interest:If you deposit<mark>"+principal+"</mark>,<br/>"+
+   "at an interest rate of <mark>"+ rate + "</mark>,<br/>"+
+   "you will receive an amount of <mark>" + interest +"</mark>,<br/>"+
+   "in the year <mark>" + yearInFuture + "</mark>";
+}
+//update the value
+function getSliderValue()
 {
-    p = document.getElementById("principal").value;
-    
+    document.getElementById("rateSpan").innerHTML=document.getElementById("rate").value;
+
+}
+//check for positive nos
+function validateAmount()
+{
+    var principal = document.getElementById("principal").value;
+    var biggerThanZero=parseInt(principal)>0;
+    if(!biggerThanZero)
+    {
+        alert("Enter positive number");
+        document.getElementById("principal").focus();
+    }
 }
         
